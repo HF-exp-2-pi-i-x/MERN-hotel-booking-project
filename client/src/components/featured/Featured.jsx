d97e94684aa76162a57f1715dd4537afb4f50c40
HF-exp-2-pi-i-x <106465514+HF-exp-2-pi-i-x@users.noreply.github.com>
2023-04-21 18:20:28 +0800

    finish featured ui

---
+++ client/src/components/featured/Featured.jsx
@@ -0,0 +1,42 @@
+import berlin from "../../assets/berlin.jpg";
+import kyoto from "../../assets/kyoto.jpg";
+import london from "../../assets/london.jpg";
+import "./featured.scss";
+
+const Featured = () => {
+  return (
+    <div className="row mt-3">
+      <div className="col">
+        <div className="card bg-dark text-white" >
+          <img src={berlin}
+          className="card-img"
+           alt="..." />
+          <div className="card-img-overlay">
+            <h1 className="card-title text-center">Berlin</h1>
+            <p className="card-text text-center">555 properties</p>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="card bg-dark text-white" >
+          <img src={london}className="card-img" alt="..." />
+          <div className="card-img-overlay">
+            <h1 className="card-title text-center">London</h1>
+            <p className="card-text text-center">99 properties</p>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="card bg-dark text-white" >
+          <img src={kyoto}className="card-img " alt="..." />
+          <div className="card-img-overlay">
+            <h1 className="card-title text-center">Kyoto</h1>
+            <p className="card-text text-center">562 properties</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Featured;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,13 +1,15 @@
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
+import Featured from "../../components/featured/Featured";
+
 
 const Home = ()=>{
   return (
    <div>
       <Navbar/>
       <Header/>
-      <div className="home-container d-flex flex-col">
-        <p>hello</p>
+      <div className="home-container container-fluid">
+       <Featured/>
       </div>
    </div>
   )
